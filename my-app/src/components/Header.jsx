4c751a43ae197f4849c22438a0b08311ea4d969d
athaarnaqvi <i221166@nu.edu.pk>
2025-04-27 17:41:00 +0500

    Added Summary testing, icon and logo

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 import { AiOutlinePlus, AiOutlineUser } from "react-icons/ai";
+import { IoLibraryOutline } from "react-icons/io5";
 import "../main.css";
 
 const Header = ({ toggleTheme, isDarkMode, openModal }) => {
   return (
     <header className="header">
-      <h1 className="header-title">BookHaven</h1>
+      <h1 className="header-title"><IoLibraryOutline  style={{ marginBottom: '-2px'}}/> BookHaven</h1>
       <div className="header-buttons">
         {/* Add Book Button */}
         <button onClick={openModal}>
